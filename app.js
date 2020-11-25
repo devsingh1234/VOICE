c8fb8371d97cded0aed6725c13700412c4926a70
devsingh1234 <52194749+devsingh1234@users.noreply.github.com>
2020-11-25 10:45:06 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const btn = document.querySelector('.talk');
+const content = document.querySelector('.content')
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+recognition.onstart = function() {
+    console.log('You can speak now!!!');
+}
+
+recognition.onresult = function(event) {
+
+    const current = event.resultIndex;
+
+    const transcript = event.results[current][0].transcript;
+    content.textContent = transcript;
+    readOutLoud(transcript);
+
+
+};
+
+
+//addd the listner
+
+btn.addEventListener('click', () => {
+    recognition.start();
+});
+
+
+function readOutLoud(message) {
+    const speech = new SpeechSynthesisUtterance();
+
+    speech.text = message;
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+
+    window.SpeechSynthesis.speak(speech);
+}
